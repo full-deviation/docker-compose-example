6ef1a847b3b6098a9249b45f2f778e9e98a0c1a0
full-deviation <mimic.pandora@gmail.com>
2022-10-09 23:19:07 +0900

    RemoteSigned管理権限でやるのよくないよね

--- backup.bat
+++ backup.bat
@@ -1,5 +1,5 @@
 cd %~dp0
 cd pukiwiki
-powershell -File backup.ps1
+powershell -ExecutionPolicy RemoteSigned -File backup.ps1
 cd ..\gitbucket
-powershell -File backup.ps1
+powershell -ExecutionPolicy RemoteSigned -File backup.ps1
